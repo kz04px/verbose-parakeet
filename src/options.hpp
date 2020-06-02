00a4354754115c1f4d06c7345a936777a51b514b
kz04px <a>
2020-06-02 19:24:17 +0100

    Implement options
    None of these do anything yet.

---
+++ src/options.hpp
@@ -0,0 +1,30 @@
+#ifndef OPTIONS_HPP
+#define OPTIONS_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+
+struct Spin {
+    std::string name;
+    int min;
+    int max;
+    int val;
+};
+
+struct Combo {
+    std::string name;
+    std::size_t selected;
+    std::vector<std::string> choices;
+};
+
+namespace options {
+
+extern Spin hash;
+extern Spin threads;
+extern Spin contempt;
+extern Combo test;
+
+}  // namespace options
+
+#endif
