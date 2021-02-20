a67c99aacdad5cd01c442d62477085390f985267
kz04px <a>
2021-02-20 15:27:16 +0000

    Add a random player

---
+++ src/search/random/search.hpp
@@ -0,0 +1,22 @@
+#ifndef SEARCH_RANDOM_HPP
+#define SEARCH_RANDOM_HPP
+
+#include <libchess/move.hpp>
+#include <libchess/position.hpp>
+#include "../search.hpp"
+
+namespace rando {
+
+search_return search(libchess::Position pos, const info_printer info) {
+    const auto moves = pos.legal_moves();
+
+    if (!moves.empty()) {
+        return {moves.at(rand() % moves.size()), {}};
+    }
+
+    return {};
+}
+
+}  // namespace rando
+
+#endif
