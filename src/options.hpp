b20fcb778cb235945c02e4677225caa27b6b317c
kz04px <6536046+kz04px@users.noreply.github.com>
2021-02-20 05:07:27 +0000

    Set the Combo option type properly

--- src/options.hpp
+++ src/options.hpp
@@ -16,6 +16,20 @@ struct Combo {
     std::string name;
     std::size_t selected;
     std::vector<std::string> choices;
+
+    const auto set(const std::string &option) noexcept {
+        for (std::size_t i = 0; i < choices.size(); ++i) {
+            if (choices[i] == option) {
+                selected = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    [[nodiscard]] const auto &get() const {
+        return choices.at(selected);
+    }
 };
 
 namespace options {
