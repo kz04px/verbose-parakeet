328b9e3f69bb5d9deefeb9f47ad850fc98c4b0ee
kz04px <a>
2021-02-21 14:33:42 +0000

    Put player options in the "player" namespace
    Please save us from name collisions

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -1,5 +1,9 @@
 #include "search.hpp"
 
+namespace player {
+
 search_type search = [](libchess::Position pos, const info_printer info) -> search_return { return {}; };
 
 std::function<int(const libchess::Position &pos)> eval = [](const libchess::Position &pos) { return 0; };
+
+}  // namespace player
